97244d29d6990a7b6c93943d42e2cf97bf9943bd
Wang Fei <fisher.wang@t-systems.com>
2024-08-22 18:34:36 +0800

    tuto

--- bin/tmp.py
+++ bin/tmp.py
@@ -1,8 +1,9 @@
-# import os
+import os
 # os.system('C:\\Users\\a200197817\\Apps\\Xunlian\\fotiaoqiang.exe')
+os.spawnl(os.P_DETACH, 'C:\\Users\\a200197817\\Apps\\Xunlian\\fotiaoqiang.exe', ' ')
 
-import subprocess
-subprocess.Popen('C:\\Windows\\System32\\notepad.exe')
+# import subprocess
+# subprocess.Popen('C:\\Windows\\System32\\notepad.exe')
 
 # os.system('reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL /t REG_SZ /d "http://proxy-cn.t-systems.com/proxy.pac" /f')
 # os.system('reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f')
